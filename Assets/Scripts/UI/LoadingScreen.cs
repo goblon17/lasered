1234c1bc5bf145fdbb1a5cf8c3cc12b172a016e3
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2023-09-17 01:03:56 +0200

    Add tips to loading screen

--- Assets/Scripts/UI/LoadingScreen.cs
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -6,16 +6,53 @@ using TMPro;
 public class LoadingScreen : MonoBehaviour
 {
     [SerializeField]
+    private List<string> tips;
+    [SerializeField]
+    private float tipShowDuration;
+    [SerializeField]
+    private float tipFadeDuration;
+    [SerializeField]
     private TextMeshProUGUI text;
+    [SerializeField]
+    private TextMeshProUGUI tipText;
+
+    private Coroutine coroutine = null;
+    private int index = 0;
 
     public void Show(string msg)
     {
         text.text = msg;
         gameObject.SetActive(true);
+        index = Random.Range(0, tips.Count);
+        tipText.text = tips[index];
+        tipText.color = PlayerData.PlayerColorToColor((PlayerData.PlayerColor)Random.Range(0, System.Enum.GetValues(typeof(PlayerData.PlayerColor)).Length));
+        StartCoroutine(TipCoroutine());
+    }
+
+    private IEnumerator TipCoroutine()
+    {
+        while (gameObject.activeSelf)
+        {
+            yield return new WaitForSeconds(tipShowDuration);
+
+            yield return UIUtils.TextFadeCoroutine(tipText, tipFadeDuration, UIUtils.Fade.FadeOut);
+
+            index++;
+            index %= tips.Count;
+            tipText.text = tips[index];
+            tipText.color = PlayerData.PlayerColorToColor((PlayerData.PlayerColor)Random.Range(0, System.Enum.GetValues(typeof(PlayerData.PlayerColor)).Length));
+
+            yield return UIUtils.TextFadeCoroutine(tipText, tipFadeDuration, UIUtils.Fade.FadeIn);
+        }
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
     }
 }
